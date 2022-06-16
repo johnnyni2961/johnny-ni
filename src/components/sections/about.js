9311e0332c79481aa1cf9be1a1129eded693a0c5
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-16 12:28:13 -0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,33 +135,30 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              What's up! I'm Johnny Ni, a guy who loves building, inventing cool stuff, and drinking coffee. My
+              interests fall into software engineering, entrepreneurship, web3, and defying society.
+              I love networking and meeting with some of the chillest engineers and folks out there, I even
+              launched a startup just for Gen-Z networking... HMU if you wanna talk or just have fun.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Here's what I've been up to... I’m currently working on{' '}
+              <a href="https://asoshi-landing.vercel.app/">launching a tech startup</a>,{' '}
+              <a href="https://www.northropgrumman.com/">a huge corporation</a>,{' '}
+              <a href="https://a16z.com/">crypto Venture Capital</a>, and{' '}
+              <a href="https://oval.cs.stanford.edu/">research</a>. My
+              main priority at the moment is at launching <a href="https://asoshi-landing.vercel.app/">ASOSHI</a>, a networking platform for Gen-Z innovators. Currently shipping product &amp; raising seed-round. 
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              Recently, I've also been involved in a web3 gaming ecosystem, {' '}
+              <a href="https://joystickgames.com/">
+                JoyStick
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              that empowers players to make money playing games they love. (Raised a $8 million USD seed round!)
             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here’s a few skills I've been working on:</p>
           </div>
 
           <ul className="skills-list">
