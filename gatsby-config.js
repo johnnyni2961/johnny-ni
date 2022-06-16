1ce8b6c45bc35d97526755ee26164c7412fae9c5
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-16 14:03:23 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Johnny Ni',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Johnny Ni is a student who loves building, scaling startups, drinking coffee, and having fun.',
+    siteUrl: 'https://johnny-ni.vercel.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@JohnnyNi13',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'JohnnyNi',
+        short_name: 'JohnnyNi',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
