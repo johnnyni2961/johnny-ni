58d92e6ada7ded9cf712b42dace0ead799094f23
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-06-16 12:29:26 -0700

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,16 +59,15 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Johnny Ni.</h2>;
+  const three = <h3 className="big-heading">Innovating &amp; building tomorrow's software.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a startup founder, software engineer, web3 adventurer, cybersecurity enthusiast, and student currently focused on student networking, Decentralized Autonomous Organizations, and industry software.
+        Currently working with machine learning software at {' '}
+        <a href="https://www.northropgrumman.com/" target="_blank" rel="noreferrer">
+          Northrop Grumman
         </a>
         .
       </p>
@@ -77,10 +76,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://even-road-d0e.notion.site/Contact-Johnny-36ff29e98e304c76a2af04b45a21f9cf"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Reach out to me here!
     </a>
   );
 
